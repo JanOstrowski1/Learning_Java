eb5765d17b8f6af3663132180d96dc0c90c00459
JanOstrowski1 <40797574+JanOstrowski1@users.noreply.github.com>
2019-04-06 23:26:48 +0200

    test

---
+++ src/com/company/ZadanieMain.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ZadanieMain {
+    public static void main(String[] args) {
+        ArrayList list= new ArrayList<String>(Arrays.asList("hello","academy","java","junior"));
+
+
+    }
+
+}
