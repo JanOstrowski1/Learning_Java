0c12d07384bf91e4bf302c7eedb328dbccf3141a
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2019-04-19 16:44:44 +0200

    2 zadanie prawie skonczone

--- Homework_5/task5/src/main/java/servlet/KindServlet.java
+++ Homework_5/task5/src/main/java/servlet/KindServlet.java
@@ -11,7 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -19,18 +22,34 @@ import java.util.Map;
 public class KindServlet extends HttpServlet {
     @Inject
     TemplateProvider templateProvider ;
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Enumeration enumeration= req.getParameterNames();
+        while (enumeration.hasMoreElements()) {
+            String param = (String) enumeration.nextElement();
+            String value = req.getParameter(param);
+            resp.getWriter().println(value);
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String nameAndSurname="Jan Ostrowski";
         String group = "jjdd6-codeina" ;
 
+        LocalDateTime time = LocalDateTime.now();
+//
 //        resp.getWriter().println(nameAndSurname);
 //        resp.getWriter().println(group);
         Template template = templateProvider.getTemplate(getServletContext(),"new.ftlh");
         Map<String,Object> model = new HashMap<>();
         model.put("name",nameAndSurname);
         model.put("group",group);
+        model.put("time",time);
         try {
             template.process(model,resp.getWriter());
         } catch (TemplateException e) {
